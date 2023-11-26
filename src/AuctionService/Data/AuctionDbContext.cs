c5127355ae6b2bead2fbb8ee3d965769ac796cd2
Fouche du Preez <fouche@limehouse.co.za>
2023-11-26 21:48:28 +0200

    init

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Auction> Actions { get; set; }
+    
+}
