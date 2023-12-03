f6df4c2bb34e765208e90aa81beecb1c33bbb928
Fouche du Preez <fouche@limehouse.co.za>
2023-12-03 20:57:09 +0200

    Added DB seed and DTOs

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -9,11 +9,23 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+// Register all classes that extends Profile
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
 app.UseAuthorization();
 app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
 app.Run();
 
 
